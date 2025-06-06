f813cfecb6b04aa9b22462cc7facb23bb1d2626c
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-06-07 00:51:25 +0200

    first commit

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,37 @@
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+
+    public static void main(String[] args) {
+
+        try (Scanner input = new Scanner(System.in);) {
+            System.out.println("Inserisci il tuo nome:");
+            String firstName = input.nextLine();
+
+            System.out.println("Inserisci il tuo cognome:");
+            String lastName = input.nextLine();
+
+            System.out.println("Inserisci il tuo colore preferito:");
+            String color = input.nextLine();
+
+            System.out.println("Inserisci il tuo giorno di nascita:");
+            int day = input.nextInt();
+            input.nextLine();
+
+            System.out.println("Inserisci il tuo mese di nascita");
+            int month = input.nextInt();
+            input.nextLine();
+
+            System.out.println("Inserisci il tuo anno di nascita");
+            int year = input.nextInt();
+            input.nextLine();
+
+            System.out.println("La tua password supersicura e':" + firstName.toLowerCase().replaceAll(" ", "-") + "-"
+                    + lastName.toLowerCase().replaceAll(" ", "-")
+                    + "-" + color.toLowerCase().replaceAll(" ", "-") + "-" + (day + month + year));
+        }
+    }
+
+}
